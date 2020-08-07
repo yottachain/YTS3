b7272a6b4b1fa09a211e774e5338f07d9242b263
penghuaisong <Aa111111>
2020-08-07 18:08:51 +0800

    文件上传接口

--- controller/UploaderController.go
+++ controller/UploaderController.go
@@ -1,1 +1,59 @@
 package controller
+
+import (
+	"fmt"
+	"os"
+	"path"
+	"path/filepath"
+	"time"
+
+	"github.com/ethereum/go-ethereum/log"
+	"github.com/gin-gonic/gin"
+	"github.com/yottachain/YTCoreService/api"
+)
+
+//UploadFile 根据路径上传文件
+func UploadFile(g *gin.Context) string {
+
+	bucketName := g.PostForm("bucketName")
+
+	publicKey := g.PostForm("publicKey")
+	filepath := g.PostForm("path")
+
+	var filename string
+	filename = path.Base(filepath)
+	fmt.Println("filename=", filename)
+
+	c := api.GetClient(publicKey)
+	//根据路径上传文件
+	upload := c.NewUploadObject()
+	hash, err := upload.UploadFile(filepath)
+	if err != nil {
+
+	}
+	var header map[string]string
+	header = make(map[string]string)
+	timeUnix := time.Now().Unix()
+	fileSize := getFileSize(filepath)
+	header["ETag"] = string(hash)
+	header["x-amz-date"] = string(timeUnix)
+	header["contentLength"] = string(fileSize)
+	meta, err1 := api.FileMetaMapTobytes(header)
+	if err1 != nil {
+		log.Error(err1.Error())
+	}
+	//写元数据
+	c.NewObjectAccessor().CreateObject(bucketName, filename, upload.VNU, meta)
+
+	//如果成功返回文件hash
+	return string(hash)
+}
+
+func getFileSize(filename string) int64 {
+	var result int64
+	filepath.Walk(filename, func(path string, f os.FileInfo, err error) error {
+		result = f.Size()
+		return nil
+	})
+	return result
+}
