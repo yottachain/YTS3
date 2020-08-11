96c97fefb6bc45c0ac8a9cb4dd88ca69ccf169e8
penghuaisong <Aa111111>
2020-08-11 17:03:47 +0800

    update

--- controller/UserController.go
+++ controller/UserController.go
@@ -9,25 +9,33 @@ import (
 	"github.com/yottachain/YTCoreService/api"
 )
 
+//User 用户注册
+type User struct {
+	UserName   string `form:"userName" json:"userName" binding:"required"`
+	PrivateKey string `form:"privateKey" json:"privateKey" xml:"privateKey" binding:"required"`
+}
+
 //Register 用户注册
 func Register(g *gin.Context) {
 
-	// var count int = 0
+	var json User
 
-	userName := g.PostForm("userName")
+	if err := g.Bind(&json); err != nil {
+		g.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+	}
+	userName := json.UserName
 
-	privateKey := g.PostForm("privateKey")
-
+	privateKey := json.PrivateKey
 	// log.Info("userName : " + userName)
 	// log.Info("privateKey : " + privateKey)
 
 	// if count == 0 {
-	c, err := api.NewClient(userName, privateKey)
-	if err != nil {
-		CheckErr(err)
+	c, err2 := api.NewClient(userName, privateKey)
+	if err2 != nil {
+		CheckErr(err2)
 	}
 	log.Info("User Register Success,UserName:" + "testnodeyyy1")
 	fmt.Println("UserID:", c.UserId)
 	// }
-	g.String(http.StatusOK, "User Register Success,UserName:"+c.Username)
+	g.JSON(http.StatusOK, gin.H{"status": "Register success " + userName})
 }
