018d91bb65ee57bf97467ad9e23e5fc349424f00
zhangzhengyan <zzy_19831106@163.com>
2021-01-19 17:57:00 +0800

    update

--- controller/HttpServerController.go
+++ controller/HttpServerController.go
@@ -10,6 +10,6 @@ import (
 func GetProgramVersion(g *gin.Context) {
 	defer env.TracePanic("GetProgramVersion")
 	var versionID string
-	versionID = "2.0.0.21"
+	versionID = "2.0.1.0"
 	g.JSON(http.StatusOK, gin.H{"versionID": versionID})
 }
