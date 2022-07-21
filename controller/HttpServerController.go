21b7bf0b7ab7001240c2e8fc0bc89ad280df23cd
gengwenjuan <gengwenjuan@sursen.net>
2022-07-21 11:00:16 +0800

    changed version to 2.0.2.0

--- controller/HttpServerController.go
+++ controller/HttpServerController.go
@@ -10,6 +10,6 @@ import (
 func GetProgramVersion(g *gin.Context) {
 	defer env.TracePanic("GetProgramVersion")
 	//var versionID string
-	versionID := "2.0.1.7"
+	versionID := "2.0.2.0"
 	g.JSON(http.StatusOK, gin.H{"versionID": versionID})
 }
